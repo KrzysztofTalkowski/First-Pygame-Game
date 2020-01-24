774d5d0d1c288d889a56bdb56ff7e6e2f226bd02
Krzysztof Talkowski <k.talkowski@gmail.com>
2020-01-24 18:54:45 +0100

    created movement + game zone

--- Main.py
+++ Main.py
@@ -11,13 +11,54 @@ pygame.display.set_caption("Hungry Dragon")
 icon = pygame.image.load('monster.png')
 pygame.display.set_icon(icon)
 
+# Player
+playerImg = pygame.image.load('dragon.png')
+playerX = 370
+playerY = 480
+
+
+def player(x, y):
+    screen.blit(playerImg, (x, y))
+
+
 # Game Loop
 running = True
+
+
 while running:
+    # RGB        RED  GREEN  BLUE
+    screen.fill((110, 200, 100))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    # RGB        RED  GREEN  BLUE
-    screen.fill((110, 200, 100))
+
+# if keystroke is pressed check if it's right or left
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_LEFT:
+            playerX -= 0.5
+        if event.key == pygame.K_RIGHT:
+            playerX += 0.5
+        if event.key == pygame.K_UP:
+            playerY -= 0.5
+        if event.key == pygame.K_DOWN:
+            playerY += 0.5
+
+# creating game zone
+    if playerX <= 0:
+        playerX = 0
+    elif playerX >= 736:
+        playerX = 736
+
+    if playerY <= 0:
+        playerY = 0
+    elif playerY >= 536:
+        playerY = 536
+
+
+
+
+
+    player(playerX, playerY)
     pygame.display.update()
 
